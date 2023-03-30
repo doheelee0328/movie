a9f75f772736d32cae7d68cacbdd8d3d496117a9
Dohee Lee <dojuhee2015@gmail.com>
2023-03-30 23:24:53 +0100

    added a slideshow

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const arrows = document.querySelectorAll('.arrow')
+const movieList = document.querySelectorAll('.movie-list')
+console.log(movieList)
 
+arrows.forEach((arrow, i) => {
+  const movieItems = movieList[i].querySelectorAll('img').length
+  let clickCounter = 0
+  arrow.addEventListener('click', () => {
+    clickCounter++
+    if (movieItems - (4 + clickCounter) >= 0) {
+      movieList[i].style.transform = `translateX(${
+        movieList[i].computedStyleMap().get('transform')[0].x.value - 300
+      }px)`
+    } else {
+      movieList[i].style.transform = `translateX(0)`
+      clickCounter = 0
+    }
+  })
+})
