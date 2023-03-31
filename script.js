0be29803a5f9c571681b56c4e2cd540847b7f255
Dohee Lee <dojuhee2015@gmail.com>
2023-04-01 00:02:36 +0100

    added a left arrow

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const arrows = document.querySelectorAll('.arrow')
+const arrows = document.querySelectorAll('.right')
+const leftArrows = document.querySelectorAll('.left')
 const movieList = document.querySelectorAll('.movie-list')
-console.log(movieList)
 
+// Click for right arrow
 arrows.forEach((arrow, i) => {
   const movieItems = movieList[i].querySelectorAll('img').length
-  let clickCounter = 0
+  let clickRightCounter = 0
   arrow.addEventListener('click', () => {
-    clickCounter++
-    if (movieItems - (4 + clickCounter) >= 0) {
+    clickRightCounter++
+    if (movieItems - (3 + clickRightCounter) > 0) {
       movieList[i].style.transform = `translateX(${
         movieList[i].computedStyleMap().get('transform')[0].x.value - 300
       }px)`
     } else {
-      movieList[i].style.transform = `translateX(0)`
-      clickCounter = 0
+      clickRightCounter--
+      clickRightCounter = 0
+    }
+  })
+})
+
+// Click for left arrow
+leftArrows.forEach((arrow, i) => {
+  let clickLeftCounter = 0
+  arrow.addEventListener('click', () => {
+    clickLeftCounter--
+    if (clickLeftCounter >= 0.5) {
+      movieList[i].style.transform = `translateX(${
+        movieList[i].computedStyleMap().get('transform')[0].x.value + 300
+      }px)`
+    } else {
+      clickLeftCounter++
+      movieList[i].style.transform = 'translateX(0)'
+      clickLeftCounter = 0
     }
   })
 })
