cf0e6761ae335a3619e9be5a2157a9305b5a7e10
Dohee Lee <dojuhee2015@gmail.com>
2023-04-02 22:23:12 +0100

    made a few changes

--- script.js
+++ script.js
@@ -1,51 +1,29 @@
-const arrows = document.querySelectorAll('.right')
-const leftArrows = document.querySelectorAll('.left')
+const arrows = document.querySelectorAll('.arrow')
 const movieList = document.querySelectorAll('.movie-list')
-let isAnimating = false
 
-// Click for right arrow
+// Click for arrow
 arrows.forEach((arrow, i) => {
   const movieItems = movieList[i].querySelectorAll('img').length
-  let clickRightCounter = 0
+  let clickCounter = 0
   arrow.addEventListener('click', () => {
-    if (isAnimating) return // check if animation is in progress
-    isAnimating = true
-
-    clickRightCounter++
-    if (movieItems - (4 + clickRightCounter) >= 0) {
+    clickCounter++
+    if (movieItems - (5 + clickCounter) >= 0) {
       movieList[i].style.transform = `translateX(${
         movieList[i].computedStyleMap().get('transform')[0].x.value - 300
-      }px)`
-      movieList[i].addEventListener('transitionend', () => {
-        isAnimating = false // set flag to false when animation is complete
-      })
+      }px)` // moves to the right
     } else {
-      clickRightCounter = 0
-    }
-  })
-})
-
-// Click for left arrow
-leftArrows.forEach((arrow, i) => {
-  let clickLeftCounter = 0
-  arrow.addEventListener('click', () => {
-    clickLeftCounter--
-    if (clickLeftCounter >= 1) {
-      movieList[i].style.transform = `translateX(${
-        movieList[i].computedStyleMap().get('transform')[0].x.value + 300
-      }px)`
-    } else {
-      movieList[i].style.transform = 'translateX(0)'
-      clickLeftCounter = 0
+      movieList[i].style.transform = 'translateX(0)' // moving back to the start
+      clickCounter = 0 // to reset
     }
   })
 })
 
 // toggle for the dark/light mode
+const toggleItems =
+  '.container,.sidebar,#navbar,.menu-list,.profile-container,.search,.movies,.most-popular,.movie-title,.toggle,.arrow '
+
 const toggleBall = document.querySelector('.toggle-ball')
-const items = document.querySelectorAll(
-  '.container,.sidebar,#navbar,.menu-list,.profile-container,.search,.movies,.most-popular,.movie-title,.toggle,.right,.left '
-)
+const items = document.querySelectorAll(toggleItems)
 
 toggleBall.addEventListener('click', () => {
   items.forEach((item) => {
@@ -54,3 +32,5 @@ toggleBall.addEventListener('click', () => {
   })
   toggleBall.classList.toggle('active')
 })
+
+// hover effect for 2 images
